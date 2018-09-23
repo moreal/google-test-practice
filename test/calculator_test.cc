2f811a4d230757a1bd1b375b14fb0c6e3281af92
moreal <dev.moreal@gmail.com>
2018-09-23 23:27:31 +0900

    Fix test code

--- test/calculator_test.cc
+++ test/calculator_test.cc
@@ -4,7 +4,7 @@
 struct calc_state {
     int a;
     int b;
-    int result;
+    int sum, sub, mul, div, remain;
 };
 
 struct CalculatorTest : public testing::WithParamInterface<calc_state>, testing::Test{
@@ -21,29 +21,30 @@ struct CalculatorTest : public testing::WithParamInterface<calc_state>, testing:
 
 TEST_P(CalculatorTest, SumCalcTest) {
     auto as = GetParam();
-    EXPECT_EQ(as.result, calculator->Sum());
+    EXPECT_EQ(as.sum, calculator->Sum());
 }
 
 TEST_P(CalculatorTest, SubCalcTest) {
     auto as = GetParam();
-    EXPECT_EQ(as.result, calculator->Sub());
+    EXPECT_EQ(as.sub, calculator->Sub());
 }
 
 TEST_P(CalculatorTest, DivCalcTest) {
     auto as = GetParam();
-    EXPECT_EQ(as.result, calculator->Div());
+    EXPECT_EQ(as.div, calculator->Div());
 }
 
 TEST_P(CalculatorTest, MulCalcTest) {
     auto as = GetParam();
-    EXPECT_EQ(as.result, calculator->Mul());
+    EXPECT_EQ(as.mul, calculator->Mul());
 }
 
 TEST_P(CalculatorTest, RemainCalcTest) {
     auto as = GetParam();
-    EXPECT_EQ(as.result, calculator->Remain());
+    EXPECT_EQ(as.remain, calculator->Remain());
 }
 
 INSTANTIATE_TEST_CASE_P(Default, CalculatorTest,
         testing::Values(
-                calc_state {1, 1, 2}, calc_state {1, 1, 2}));
+                calc_state {1, 1, 2, 0, 1, 1, 0},
+                calc_state {2, 5, 7, -3, 10, 0, 2}));
